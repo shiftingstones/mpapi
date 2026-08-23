c783ed7d967709c175ae2fa417780131f3092203
shiftingstones <317486341+shiftingstones@users.noreply.github.com>
2026-08-23 16:53:23 -0400

    Add logging to the console and a JSON file

---
+++ src/core/logging.py
@@ -0,0 +1,56 @@
+"""A module that defines the logging configuration used throughout the app."""
+
+import logging
+from logging.handlers import RotatingFileHandler
+from fastapi import Request
+from asgi_correlation_id.log_filters import CorrelationIdFilter
+from pythonjsonlogger.json import JsonFormatter
+from http import HTTPStatus
+
+MPAPI_LOG_FILE = "/var/log/mpapi.log"
+
+# Configure the root logger
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# Log the first 8 characters of the correlation ID with every request
+correlation_log_filter = CorrelationIdFilter(uuid_length=8, default_value="-")
+logger.addFilter(correlation_log_filter)
+
+# The handler and formatter to show output in the console window
+console_log_handler = logging.StreamHandler()
+console_log_formatter = logging.Formatter(
+    fmt="%(levelname)s:\t\b%(asctime)s %(name)s [%(correlation_id)s] %(message)s"
+)
+console_log_handler.setFormatter(console_log_formatter)
+logger.addHandler(console_log_handler)
+
+# The handle and formatter to log messages to a JSON file
+file_log_handler = RotatingFileHandler(
+    MPAPI_LOG_FILE, maxBytes=10 * 1024 * 1024, backupCount=5, encoding="utf-8"
+)
+file_log_formatter = JsonFormatter(
+    fmt=["asctime", "correlation_id", "levelname", "name", "message"],
+    rename_fields={"asctime": "timestamp", "levelname": "level", "name": "logger"},
+)
+file_log_handler.setFormatter(file_log_formatter)
+logger.addHandler(file_log_handler)
+
+
+async def log_requests_middleware(request: Request, call_next):
+    """Middleware used to log every access request."""
+
+    response = await call_next(request)
+
+    # Combine the response code with its corresponding "user friendly" description
+    http_status = HTTPStatus(response.status_code)
+    response_status = f"{http_status.value} {http_status.phrase}"
+
+    # Client host and port number
+    client_host = request.client.host if request.client else ""
+    client_port = request.client.port if request.client else ""
+    client = f"{client_host}:{client_port}"
+
+    logger.info(f"{client} '{request.method} {request.url}' {response_status}")
+
+    return response
--- src/main.py
+++ src/main.py
@@ -2,15 +2,20 @@
 
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from starlette.middleware.base import BaseHTTPMiddleware
 from asgi_correlation_id import CorrelationIdMiddleware
 from routers.starship import router as starship_router
 from dependencies.correlation import CORRELATION_HEADER
+from core.logging import log_requests_middleware
 
-# Endpoints are defined in separate modules under the routers package.
+# Endpoints are defined in separate modules under the routers package
 app = FastAPI()
 app.include_router(starship_router)
 
-# Add middleware to accept (or generate) a correlation ID with every request.
+# Add middleware to log all requests
+app.add_middleware(BaseHTTPMiddleware, dispatch=log_requests_middleware)
+
+# Add middleware to accept (or generate) a correlation ID with every request
 app.add_middleware(CorrelationIdMiddleware, header_name=CORRELATION_HEADER)
 
 
